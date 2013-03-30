5df79286f4d8d15ee227029f830d2976e868824b
px-amaac <amcintyre2@mail.csuchico.edu>
2013-03-30 12:27:06 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Appdomumsite::Application.config.secret_token = '2bae3a238ef832bcf518478de944e4596afe4096c87807511b90c8d42d29cbfc4aa2928caa8624bdd5f7b4863de7a3e7cdf0a5fcf9b671607baaf2b868417dce'
