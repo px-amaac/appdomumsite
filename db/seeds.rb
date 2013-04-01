2fd7c7f554b017117a2a287aeee97098be29a41d
px-amaac <amcintyre2@mail.csuchico.edu>
2013-03-31 17:44:52 -0700

    added another admin and changed credentials

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,3 +16,6 @@ puts 'DEFAULT USERS'
 user = User.find_or_create_by_email :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
 puts 'user: ' << user.name
 user.add_role :admin
+user2 = User.find_or_create_by_email :name => ENV['CHRIS'].dup, :email => ENV['CHRIS_EMAIL'].dup, :password => ENV['CHRIS_PASSWORD'].dup, :password_confirmation => ENV['CHRIS_PASSWORD'].dup
+puts 'user: ' << user2.name
+user2.add_role :admin
