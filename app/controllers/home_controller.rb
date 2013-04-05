5b3c6697e32d94399d4157176dc1c9e83533f5d3
px-amaac <amcintyre2@mail.csuchico.edu>
2013-04-05 00:46:13 -0700

    add bio edit functions

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,11 +2,9 @@ class HomeController < ApplicationController
   def index
     @users = User.all
     @androiddev = User.first
+    @iphonedev = User.find_by_id(2)
     if user_signed_in?
 	    @microblog = current_user.microblogs.build
 	end
-    
-
-
   end
 end
