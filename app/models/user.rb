7fe1578819377de051019f8f6c15240b59a84fae
px-amaac <amcintyre2@mail.csuchico.edu>
2013-04-15 11:33:09 -0700

    ready to launch

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :role_ids, :as => :admin
-  attr_accessible :name, :email, :password, :password_confirmation, :remember_me, :user_bio, :user_image
+  attr_accessible :name, :email, :password, :password_confirmation, :remember_me, :user_bio, :user_image, :github_link
   
 
 
