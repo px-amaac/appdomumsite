414a9ca3022a300750ec1c5292128522e7f29666
px-amaac <amcintyre2@mail.csuchico.edu>
2013-04-25 17:17:47 -0700

    fixed buggs with images and added google play and apple app store. icons

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-Appdomumsite::Application.configure do
+                                                                    Appdomumsite::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # In the development environment your application's code is reloaded on
